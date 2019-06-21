309c3c108c072977ea7aa8aadc1d75fb2fe778cf
Molisawa <36557566+Molisawa@users.noreply.github.com>
2019-06-21 16:07:31 -0500

    chat ionic
    version 1.0

---
+++ chatOnline/src/app/home/home.page.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../servicios/auth.service";
+import { ChatService, chat } from "../servicios/chat.service";
+import { ModalController } from "@ionic/angular";
+import { ChatComponent } from "../componentes/chat/chat.component";
+import { ActionSheetController } from '@ionic/angular';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+
+  public chatRooms : any = [];
+
+
+  constructor(public authservice : AuthService,
+              public chatservice : ChatService,
+              private modal : ModalController,
+              public actionSheetController: ActionSheetController) {}
+  OnLogout(){
+    this.authservice.logout();
+  }
+
+  ngOnInit(){
+    this.chatservice.getChatRoomss().subscribe( chats => {
+
+      this.chatRooms = chats;
+    })
+  }
+
+
+  openChat(chat){
+    this.modal.create({
+      component : ChatComponent,
+      componentProps : {
+        chat: chat
+      }
+    }).then( (modal) => modal.present())
+  }
+
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Opciones',
+      buttons: [{
+        text: 'Desconectarse',
+        role: 'destructive',
+        icon: 'log-aout',
+        handler: () => {
+          this.OnLogout()
+        },
+      }]
+    });
+    await actionSheet.present();
+  }
+
+
+}
---
+++ chatOnline/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD0n9QUt6iGG6KVobwn-kGkqNoYZQWin3E",
+  authDomain: "chatonline-e53f7.firebaseapp.com",
+  databaseURL: "https://chatonline-e53f7.firebaseio.com",
+  projectId: "chatonline-e53f7",
+  storageBucket: "chatonline-e53f7.appspot.com",
+  messagingSenderId: "914032477925",
+  appId: "1:914032477925:web:f825c4ff07375b72"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
